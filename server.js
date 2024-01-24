b245e4d472c9d209827c22fcf1424bcacaf12797
Xereng <138768675+Xereng@users.noreply.github.com>
2024-01-24 16:37:46 +0000

    nothing changed

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const bodyParser =require('body-parser');
+
+const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+mongoose.connect('', {useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log('MongoDB Connected')).catch(err=> console.log(err));
 
-    console.log(`server is running on port ${PORT}`);
+app.use(bodyParser.json());
+
+app.get('/api/example',(req,res)=>{
+
+    res.json({
+        message: 'Hello from the server!' });
 });
+
+app.use(express.static(path.join(__dirname, 'client','public','src')));
+
+app.get('*', (req, res) =>{
+
+    res.sendFile(path.join(__dirname, 'client','public','src','index.html'));
+});
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
